4d9b12a87c894308fcaac7d5a54875ab31425d41
Jiéyra De Châtillon <seigneur.noir15@gmail.com>
2026-02-04 10:19:43 +0700

    deploying api

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   app.useLogger(logger);
