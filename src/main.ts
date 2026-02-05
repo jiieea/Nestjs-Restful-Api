96488d66f343934acef87b3cfd0a8dd04af96669
Jiéyra De Châtillon <seigneur.noir15@gmail.com>
2026-02-05 08:56:31 +0700

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { configure as serverlessExpress } from '@codegenie/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
+import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
-let server: Handler;
-
-async function bootstrap(): Promise<Handler> {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Apply your global configs here (pipes, interceptors, etc.)
-  app.enableCors();
-
-  await app.init();
-
-  const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress({ app: expressApp });
+  // Important: Railway provides a PORT variable automatically
+  const port = process.env.PORT || 3000;
+  const logger = app.get(WINSTON_MODULE_NEST_PROVIDER);
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  app.useLogger(logger);
+  // Use '0.0.0.0' to ensure Railway can route traffic to the container
+  await app.listen(port, '0.0.0.0');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
-
-// The Vercel entry point
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  server = server ?? (await bootstrap());
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-  return server(event, context, callback);
-};
+bootstrap();
