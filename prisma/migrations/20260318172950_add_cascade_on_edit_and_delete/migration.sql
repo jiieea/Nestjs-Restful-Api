07cf0c00b227625c38b9d999faa6c01e75e8b5c0
Jiéyra De Châtillon <seigneur.noir15@gmail.com>
2026-03-19 00:39:17 +0700

    update cascading changes

---
+++ prisma/migrations/20260318172950_add_cascade_on_edit_and_delete/migration.sql
@@ -0,0 +1,17 @@
+-- DropForeignKey
+ALTER TABLE `addresses` DROP FOREIGN KEY `addresses_contact_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `contacts` DROP FOREIGN KEY `contacts_username_fkey`;
+
+-- DropIndex
+DROP INDEX `addresses_contact_id_fkey` ON `addresses`;
+
+-- DropIndex
+DROP INDEX `contacts_username_fkey` ON `contacts`;
+
+-- AddForeignKey
+ALTER TABLE `contacts` ADD CONSTRAINT `contacts_username_fkey` FOREIGN KEY (`username`) REFERENCES `users`(`username`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `addresses` ADD CONSTRAINT `addresses_contact_id_fkey` FOREIGN KEY (`contact_id`) REFERENCES `contacts`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
